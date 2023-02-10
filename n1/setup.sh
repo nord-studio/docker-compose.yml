91c514d941806e561aa1e6caf2e4a3d187c98ab6
Ty Mason <tygerxqt@proton.me>
2023-02-10 17:39:35 +0000

    Fix typo

--- n1/setup.sh
+++ n1/setup.sh
@@ -29,7 +29,7 @@ else
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get update > /dev/null
-    apt-get install docker-ce ddcker-ce-cli containerd.io docker-compose-plugin > /dev/null
+    apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin > /dev/null
 fi
 
 
